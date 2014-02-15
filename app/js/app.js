44352e5fc87908150a9616b3965d2ecfe4db9610
Eugene Safronov <sejoker@gmail.com>
2014-02-15 19:33:06 +0200

    moved all app logic into Marionette.Controller

--- app/js/app.js
+++ app/js/app.js
@@ -20,173 +20,25 @@ ContactManager.addInitializer(function(){
 		contacts = new ContactManager.Collections.Contacts(),
 		user = new ContactManager.Models.User(),
 		router = new ContactManager.Router(),
-	 	logon = function(callback, callbackArgs){
-			var model = new ContactManager.Models.User();
+	 	controller = new ContactManager.Controller({
+	 		contacts: contacts,
+	 		router: router,
+	 		mainRegion: this.mainRegion,
+	 		user: user,
+	 		api: api
+	 	});
 
-			model.set('createNew', false);
+	router.on('route:home', controller.home, controller);
 
-			var loginForm = new ContactManager.Views.SignupForm({
-				model: model
-			});
+	router.on('route:showContacts', controller.showContacts, controller);
 
-			loginForm.on('form:submitted', function(attrs){
-				api.loginAsync(attrs)
-				.then(function(result){
-					var signupResult = JSON.parse(result);
-					user.set('signedIn', true);
-					user.set('token', signupResult.token);
+	router.on('route:signupUser', controller.signupUser, controller);
 
-					if (callback){
-						callback(callbackArgs);
-					} else {
-						router.navigate('contacts', true);		
-					}
-				})
-				.fail(function(error){
-					model.set('errorMessage', JSON.parse(error).error);
-				});
-				
-			});
+	router.on('route:loginUser', controller.logon, controller);
 
-			ContactManager.mainRegion.show(loginForm);
-		},
+	router.on('route:newContact', controller.newContact, controller);
 
-		editContact = function(id){
-			if (user && user.get('signedIn')){
-				var contact = contacts.get(id),
-					editContactForm;
+	router.on('route:editContact', controller.editContact, controller);	
 
-				if (contact){
-					if (!contact.get('loadedDetails')){
-						api.getContactDetails(id, user.get('token')).then(function(attrs){
-							contact.set(attrs);
-
-							editContactForm = new ContactManager.Views.ContactForm({
-								model: contact
-							});
-
-							editContactForm.on('form:submitted', function(attrs){									
-								contact.set(attrs);
-								router.navigate('contacts', true);
-							})
-
-							ContactManager.mainRegion.show(editContactForm);
-						});
-					} else {
-						editContactForm = new ContactManager.Views.ContactForm({
-							model: contact
-						});
-
-						editContactForm.on('form:submitted', function(attrs){									
-							contact.set(attrs);
-							router.navigate('contacts', true);
-						})
-
-						ContactManager.mainRegion.show(editContactForm);
-					}								
-				} else {
-					router.navigate('contacts', true);
-				}
-			} else {
-				logon(editContact, id);
-			}
-		},
-		removeContact = function(id){
-			if (user && user.get('signedIn')){
-				contacts.remove(id);
-				router.navigate('contacts', true);
-			} else {
-				logon(removeContact, id);
-			}
-		}
-
-		newContact = function(){
-			if (user && user.get('signedIn')){
-				var newContactForm = new ContactManager.Views.ContactForm({
-					model: new ContactManager.Models.Contact()
-				});
-
-				newContactForm.on('form:submitted', function(attrs){
-					var contact = new ContactManager.Models.Contact();
-					contact.set('id', _.random(1, 1000));
-					contact.set(attrs);
-					contact.set('loadedDetails', true);
-					contact.set('avatar', ContactManager.Services.Api.getAvatar(attrs.gender))
-					contacts.add(contact);
-					
-					router.navigate('contacts', true);
-				});
-
-				ContactManager.mainRegion.show(newContactForm);
-			}
-			else {
-				logon(newContact);
-			}
-		};
-
-
-	router.on('route:home', function(){
-		router.navigate('contacts', {
-			trigger: true,
-			replace: true
-		});
-	});
-
-	router.on('route:showContacts', function(){
-		if (contacts.size() == 0){
-			api.getUsersAsync().then(function(users){		
-				contacts = new ContactManager.Collections.Contacts(users);					
-				var contactsView = new ContactManager.Views.Contacts({
-					collection: contacts
-				});
-
-			ContactManager.mainRegion.show(contactsView);
-
-			}).fail(function(error){
-				alert("Unexpected error occured: " + error);
-				return;
-			});
-		} else {
-			var contactsView = new ContactManager.Views.Contacts({
-					collection: contacts
-				});
-
-			ContactManager.mainRegion.show(contactsView);
-		}
-	});
-
-	router.on('route:signupUser', function(){
-		var model = new ContactManager.Models.User() 
-			signupForm = new ContactManager.Views.SignupForm({ model: model });
-
-		signupForm.on('form:submitted', function(attrs){
-			api.signupAsync(attrs)
-			.then(function(result){
-				var signupResult = JSON.parse(result);
-				user.set('signedIn', true);
-				user.set('token', signupResult.token);
-				console.log(user);
-				router.navigate('contacts', true);	
-			})
-			.fail(function(error){
-				var errors = JSON.parse(error).errors,
-					errorMessage = '';
-					_.each(errors, function(e){
-						var pairs = _.pairs(e);
-						errorMessage += pairs[0][0] + ': ' + pairs[0][1] + '<br>';
-					});
-				model.set('errorMessage', errorMessage);
-			});
-			
-		});
-
-		ContactManager.mainRegion.show(signupForm);
-	});
-
-	router.on('route:loginUser', logon);
-
-	router.on('route:newContact', newContact);
-
-	router.on('route:editContact', editContact);	
-	router.on('route:removeContact', removeContact);	
+	router.on('route:removeContact', controller.removeContact, controller);	
 });
